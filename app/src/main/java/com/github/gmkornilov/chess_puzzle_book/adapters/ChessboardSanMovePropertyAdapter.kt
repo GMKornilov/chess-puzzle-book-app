319774556252be23c6a3dbcce2c65ae724658611
fertilewaif <gmkornilov@edu.hse.ru>
2021-05-08 14:22:19 +0300

    add adapters for chessboard properties and basic fields for viewmodel

---
+++ app/src/main/java/com/github/gmkornilov/chess_puzzle_book/adapters/ChessboardSanMovePropertyAdapter.kt
@@ -0,0 +1,44 @@
+package com.github.gmkornilov.chess_puzzle_book.adapters
+
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
+import com.github.gmkornilov.chessboard.view.ChessboardView
+
+object ChessboardSanMovePropertyAdapter {
+    @InverseBindingAdapter(attribute = "fen")
+    @JvmStatic fun getFenValue(view: ChessboardView): String? {
+        return view.fen
+    }
+
+    @BindingAdapter("fen")
+    @JvmStatic fun setFenValue(view: ChessboardView, fen: String?) {
+        if (view.fen != fen) {
+            view.fen = fen
+        }
+    }
+
+    @BindingAdapter("app:fenAttrChanged")
+    @JvmStatic fun setListener(view: ChessboardView, fenAttrChanged: InverseBindingListener) {
+        view.addBoardListener(object: ChessboardView.BoardListener {
+            override fun onMove(move: String) {
+            }
+
+            override fun onFenChanged(newFen: String) {
+                fenAttrChanged.onChange()
+            }
+
+            override fun onUndo() {
+            }
+
+            override fun onCheck(isWhiteChecked: Boolean) {
+            }
+
+            override fun onCheckmate(whiteLost: Boolean) {
+            }
+
+            override fun onStalemate() {
+            }
+        })
+    }
+}
--- app/src/main/java/com/github/gmkornilov/chess_puzzle_book/ui/puzzle/PuzzleViewModel.kt
+++ app/src/main/java/com/github/gmkornilov/chess_puzzle_book/ui/puzzle/PuzzleViewModel.kt
@@ -1,6 +1,15 @@
 package com.github.gmkornilov.chess_puzzle_book.ui.puzzle
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.gmkornilov.chess_puzzle_book.data.model.Task
 
 class PuzzleViewModel : ViewModel() {
+    val isLoading: MutableLiveData<Boolean> by lazy {
+        MutableLiveData(false)
+    }
+    val task: MutableLiveData<Task?> by lazy {
+        MutableLiveData()
+    }
 }
